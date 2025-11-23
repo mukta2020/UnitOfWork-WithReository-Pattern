6499c38ae2957faad671c8a36bf20f03897e5205
mukta2020 <me.mukta@yahoo.com>
2025-11-23 14:44:35 -0600

    repository pattern

---
+++ DataAccessEF/GenericRepository.cs
@@ -0,0 +1,77 @@
+﻿using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace DataAccessEF
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly PeopleContext context;
+        public GenericRepository(PeopleContext context)
+        {
+            this.context = context;
+        }
+        public void Add(T entity)
+        {
+            context.Set<T>().Add(entity);
+        }
+        public void AddRange(IEnumerable<T> entities)
+        {
+            context.Set<T>().AddRange(entities);
+        }
+        public IEnumerable<T> Find(Expression<Func<T, bool>> expression)
+        {
+            return context.Set<T>().Where(expression);
+        }
+        public IEnumerable<T> GetAll()
+        {
+            return context.Set<T>().ToList();
+        }
+        public T GetById(int id)
+        {
+            return context.Set<T>().Find(id);
+        }
+        public void Remove(T entity)
+        {
+            context.Set<T>().Remove(entity);
+        }
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            context.Set<T>().RemoveRange(entities);
+        }
+        public virtual Task Update(T entity)
+        {
+            this.context.Set<T>().Attach(entity);
+            this.context.Entry(entity).State = EntityState.Modified;
+
+            return Task.CompletedTask;
+        }
+
+        public virtual Task UpdateRange(IEnumerable<T> entities)
+        {
+            this.context.Set<T>().AttachRange(entities);
+            this.context.Entry(entities).State = EntityState.Modified;
+
+            return Task.CompletedTask;
+        }
+        public virtual Task Detache(T entity)
+        {
+            this.context.Set<T>().Attach(entity);
+            this.context.Entry(entity).State = EntityState.Detached;
+
+            return Task.CompletedTask;
+        }
+
+        public virtual Task DetacheRange(IEnumerable<T> entities)
+        {
+            this.context.Set<T>().AttachRange(entities);
+            this.context.Entry(entities).State = EntityState.Detached;
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ DataAccessEF/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,43 @@
+﻿using DataAccessEF.TypeRepository;
+using Domain.Interfaces;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataAccessEF.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private PeopleContext context;
+        public UnitOfWork(PeopleContext context)
+        {
+            this.context = context;
+            Address = new AddressRepository(this.context);
+            Email = new EmailRepository(this.context);
+            Person = new PersonRepository(this.context);
+        }
+
+        public IAdressRepository Address { get; private set; }
+        public IEmailRepository Email { get; private set; }
+        public IPersonRepository Person { get; private set; }
+
+        ///////////////////////////
+        public async Task<int> CommitEntityAsync()
+        {
+            return await this.context.SaveChangesAsync();
+        }
+
+        public void RollbackEntityAsync()
+        {
+            this.context.ChangeTracker.Entries().ToList().ForEach(x => x.Reload());
+        }
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+
+        public int Save()
+        {
+            return context.SaveChanges();
+        }
+    }
+}
